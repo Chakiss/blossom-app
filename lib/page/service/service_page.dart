852e291c63a3aa7e143743873435fdac4d8a52b4
SongkhomSitthirat <prew.sitthirat@gmail.com>
2021-04-11 00:51:37 +0700

    Create user interface service page.

--- lib/page/service/service_page.dart
+++ lib/page/service/service_page.dart
@@ -1,11 +1,53 @@
 import 'package:blossom_clinic/base/base_screen.dart';
+import 'package:blossom_clinic/widget/blossom_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ServicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BaseScreen(
-      child: Container(),
+      child: Expanded(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          GridView.count(
+          shrinkWrap: true,
+          primary: false,
+          padding: const EdgeInsets.all(20.0),
+          crossAxisSpacing: 10.0,
+          crossAxisCount: 2,
+          mainAxisSpacing: 10.0,
+          childAspectRatio: 1 / 1,
+          children: [
+            _createServiceItem(context, "Treatment", ""),
+            _createServiceItem(context, "กระชับรูขุมขน", ""),
+            _createServiceItem(context, "ปรับรูปหน้า", ""),
+            _createServiceItem(context, "รอยดำ รอยแดง หลุมสิว", ""),
+            _createServiceItem(context, "วิตามินผิว", ""),
+            _createServiceItem(context, "กำจัดขน", ""),
+          ])
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _createServiceItem(BuildContext context, String title, String link) {
+    return Card(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SvgPicture.asset("assets/splashscreen.svg",
+                width: 30 * MediaQuery.of(context).size.width / 100,
+            height: 30 * MediaQuery.of(context).size.width / 100,),
+          ),
+          Expanded(child: Align(
+            alignment: Alignment.center,
+              child: BlossomText(title, size: 14,)))
+        ],
+      ),
     );
   }
 }
