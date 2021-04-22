de5c790f2ad30ef40b6c8c0fb5fbd095184cfcc9
SongkhomSitthirat <prew.sitthirat@gmail.com>
2021-04-22 11:18:20 +0700

    Keep detail register page.

--- lib/page/register/register_page.dart
+++ lib/page/register/register_page.dart
@@ -3,6 +3,7 @@ import 'package:blossom_clinic/widget/blossom_text.dart';
 import 'package:blossom_clinic/widget/button_pink_gradient.dart';
 import 'package:blossom_clinic/widget/text_field_stroke_black.dart';
 import 'package:blossom_clinic/widget/text_stroke_black.dart';
+import 'package:blossom_clinic/widget/toolbar.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
@@ -32,12 +33,11 @@ class _RegisterPageState extends State<RegisterPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      fit: StackFit.expand,
-      children: [
-        Scaffold(
-          backgroundColor: BlossomTheme.white,
-          body: Column(
+    return Scaffold(
+      backgroundColor: BlossomTheme.white,
+      body: Column(
+        children: [
+          Stack(
             children: [
               Align(
                 alignment: Alignment.topCenter,
@@ -46,202 +46,203 @@ class _RegisterPageState extends State<RegisterPage> {
                   child: Image.asset("assets/nav_bar.png"),
                 ),
               ),
-              Expanded(
-                  child: SingleChildScrollView(
-                child: Container(
-                  width: 80 * MediaQuery.of(context).size.width / 100,
-                  child: Column(
-                    children: [
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: BlossomText(
-                          "อีเมล",
-                          size: 16,
-                          color: BlossomTheme.black,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 6,
-                      ),
-                      TextFieldStrokeBlack(
-                        "",
-                        textController: emailTextController,
-                      ),
-                      SizedBox(
-                        height: 12,
-                      ),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: BlossomText(
-                          "รหัสผ่าน",
-                          size: 16,
-                          color: BlossomTheme.black,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 6,
-                      ),
-                      TextFieldStrokeBlack(
-                        "",
-                        textController: passwordTextController,
-                        isPasswordType: true,
-                      ),
-                      SizedBox(
-                        height: 12,
-                      ),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: BlossomText(
-                          "ยืนยันรหัสผ่าน",
-                          size: 16,
-                          color: BlossomTheme.black,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 6,
-                      ),
-                      TextFieldStrokeBlack(
-                        "",
-                        textController: confirmPasswordTextController,
-                        isPasswordType: true,
-                      ),
-                      SizedBox(
-                        height: 12,
-                      ),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: BlossomText(
-                          "เบอร์โทรศัพท์",
-                          size: 16,
-                          color: BlossomTheme.black,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 6,
-                      ),
-                      TextFieldStrokeBlack(
-                        "",
-                        textController: phoneNumberTextController,
-                        maxLength: 10,
-                      ),
-                      SizedBox(
-                        height: 12,
-                      ),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: BlossomText(
-                          "ชื่อ",
-                          size: 16,
-                          color: BlossomTheme.black,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 6,
-                      ),
-                      TextFieldStrokeBlack(
-                        "",
-                        textController: nameTextController,
-                      ),
-                      SizedBox(
-                        height: 12,
-                      ),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: BlossomText(
-                          "นามสกุล",
-                          size: 16,
-                          color: BlossomTheme.black,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 6,
-                      ),
-                      TextFieldStrokeBlack(
-                        "",
-                        textController: lastNameTextController,
-                      ),
-                      SizedBox(
-                        height: 12,
-                      ),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: BlossomText(
-                          "วันเกิด",
-                          size: 16,
-                          color: BlossomTheme.black,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 6,
-                      ),
-                      GestureDetector(
-                        behavior: HitTestBehavior.opaque,
-                        onTap: () async {
-                          final DateTime picked = await showDatePicker(
-                              context: context,
-                              initialDate: DateTime.now(),
-                              // Refer step 1
-                              firstDate: DateTime(1970),
-                              lastDate: DateTime.now(),
-                              builder: (BuildContext context, Widget child) {
-                                return Theme(
-                                  data: ThemeData.dark().copyWith(
-                                    colorScheme: ColorScheme.dark(
-                                      primary: BlossomTheme.pink,
-                                      onPrimary: BlossomTheme.black,
-                                      surface: BlossomTheme.darkPink,
-                                      onSurface: BlossomTheme.black,
-                                    ),
-                                    dialogBackgroundColor: BlossomTheme.white,
-                                  ),
-                                  child: child,
-                                );
-                              });
-                          if (picked != null)
-                            setState(() {
-                              selectedDate = picked;
-                            });
-                        },
-                        child: TextStrokeBlack(
-                          selectedDate == null ? "" : dateFormat.format(selectedDate),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 40,
-                      ),
-                      ButtonPinkGradient(
-                        "ต่อไป",
-                        true,
-                        () {
-                          _provider.goToRegisterSecondPage(
-                              context,
-                              emailTextController.text,
-                              passwordTextController.text,
-                              confirmPasswordTextController.text,
-                              phoneNumberTextController.text,
-                              nameTextController.text,
-                              lastNameTextController.text,
-                              selectedDate == null ? null : DateFormat("yyyy-MM-dd").format(selectedDate));
-                        },
-                        width: 30 * MediaQuery.of(context).size.width / 100,
-                        height: 40,
-                        radius: 6,
-                      )
-                    ],
-                  ),
-                ),
-              )),
+              SafeArea(child: Toolbar(title: "ข้อมูลผู้ใช้",)),
             ],
           ),
-        ),
-      ],
+          Expanded(
+              child: SingleChildScrollView(
+            child: Container(
+              width: 80 * MediaQuery.of(context).size.width / 100,
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: BlossomText(
+                      "อีเมล",
+                      size: 16,
+                      color: BlossomTheme.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  TextFieldStrokeBlack(
+                    "",
+                    textController: emailTextController,
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: BlossomText(
+                      "รหัสผ่าน",
+                      size: 16,
+                      color: BlossomTheme.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  TextFieldStrokeBlack(
+                    "",
+                    textController: passwordTextController,
+                    isPasswordType: true,
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: BlossomText(
+                      "ยืนยันรหัสผ่าน",
+                      size: 16,
+                      color: BlossomTheme.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  TextFieldStrokeBlack(
+                    "",
+                    textController: confirmPasswordTextController,
+                    isPasswordType: true,
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: BlossomText(
+                      "เบอร์โทรศัพท์",
+                      size: 16,
+                      color: BlossomTheme.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  TextFieldStrokeBlack(
+                    "",
+                    textController: phoneNumberTextController,
+                    maxLength: 10,
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: BlossomText(
+                      "ชื่อ",
+                      size: 16,
+                      color: BlossomTheme.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  TextFieldStrokeBlack(
+                    "",
+                    textController: nameTextController,
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: BlossomText(
+                      "นามสกุล",
+                      size: 16,
+                      color: BlossomTheme.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  TextFieldStrokeBlack(
+                    "",
+                    textController: lastNameTextController,
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: BlossomText(
+                      "วันเกิด",
+                      size: 16,
+                      color: BlossomTheme.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  GestureDetector(
+                    behavior: HitTestBehavior.opaque,
+                    onTap: () async {
+                      final DateTime picked = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          // Refer step 1
+                          firstDate: DateTime(1970),
+                          lastDate: DateTime.now(),
+                          builder: (BuildContext context, Widget child) {
+                            return Theme(
+                              data: ThemeData.dark().copyWith(
+                                colorScheme: ColorScheme.dark(
+                                  primary: BlossomTheme.pink,
+                                  onPrimary: BlossomTheme.black,
+                                  surface: BlossomTheme.darkPink,
+                                  onSurface: BlossomTheme.black,
+                                ),
+                                dialogBackgroundColor: BlossomTheme.white,
+                              ),
+                              child: child,
+                            );
+                          });
+                      if (picked != null)
+                        setState(() {
+                          selectedDate = picked;
+                        });
+                    },
+                    child: TextStrokeBlack(
+                      selectedDate == null ? "" : dateFormat.format(selectedDate),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  ButtonPinkGradient(
+                    "ต่อไป",
+                    true,
+                    () {
+                      _provider.goToRegisterSecondPage(
+                          context,
+                          emailTextController.text,
+                          passwordTextController.text,
+                          confirmPasswordTextController.text,
+                          phoneNumberTextController.text,
+                          nameTextController.text,
+                          lastNameTextController.text,
+                          selectedDate == null ? null : DateFormat("yyyy-MM-dd").format(selectedDate));
+                    },
+                    width: 30 * MediaQuery.of(context).size.width / 100,
+                    height: 40,
+                    radius: 6,
+                  )
+                ],
+              ),
+            ),
+          )),
+        ],
+      ),
     );
   }
 }
--- lib/page/register_second/register_second_page.dart
+++ lib/page/register_second/register_second_page.dart
@@ -5,6 +5,7 @@ import 'package:blossom_clinic/widget/blossom_text.dart';
 import 'package:blossom_clinic/widget/button_pink_gradient.dart';
 import 'package:blossom_clinic/widget/drug_allergy.dart';
 import 'package:blossom_clinic/widget/acne_look_radio_group.dart';
+import 'package:blossom_clinic/widget/toolbar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -37,104 +38,110 @@ class _RegisterSecondPageState extends State<RegisterSecondPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(fit: StackFit.expand, children: [
-      Scaffold(
-        backgroundColor: BlossomTheme.white,
-        body: Column(children: [
-          Align(
+    return Scaffold(
+      backgroundColor: BlossomTheme.white,
+      body: Column(children: [
+        Stack(
+          children: [
+            Align(
               alignment: Alignment.topCenter,
               child: Container(
-                  width: 100 * MediaQuery.of(context).size.width / 100, child: Image.asset("assets/nav_bar.png"))),
-          Expanded(
-              child: SingleChildScrollView(
-            child: Container(
-              width: 80 * MediaQuery.of(context).size.width / 100,
-              child: Column(
-                children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: BlossomText(
-                      "ลักษณะผิว",
-                      size: 16,
-                      color: BlossomTheme.black,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SkinLookRadioGroup((skin) {
-                    skinType = skin;
-                  }),
-                  SizedBox(
-                    height: 12,
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: BlossomText(
-                      "ลักษณะสิว",
-                      size: 16,
-                      color: BlossomTheme.black,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  AcneLookRadioGroup((acne) {
-                    acneTypes = acne;
-                  }),
-                  SizedBox(
-                    height: 12,
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: BlossomText(
-                      "เคยรักษาสิว?",
-                      size: 16,
-                      color: BlossomTheme.black,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  AcneTreat(_acneTreatTextController),
-                  SizedBox(
-                    height: 12,
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: BlossomText(
-                      "ประวัติแพ้ยา",
-                      size: 16,
-                      color: BlossomTheme.black,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 12,
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: BlossomText(
-                      "ยาที่แพ้",
-                      size: 16,
-                      color: BlossomTheme.black,
-                    ),
-                  ),
-                  DrugAllergy(_drugAllergyTextController),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  ButtonPinkGradient(
-                    "ยืนยัน",
-                    true,
-                    () {
-                      _provider.callServiceRegister(context, widget._profileData, skinType, acneTypes,
-                          _acneTreatTextController.text, _drugAllergyTextController.text);
-                    },
-                    width: 30 * MediaQuery.of(context).size.width / 100,
-                    height: 40,
-                    radius: 6,
-                  )
-                ],
+                width: 100 * MediaQuery.of(context).size.width / 100,
+                child: Image.asset("assets/nav_bar.png"),
               ),
             ),
-          )),
-        ]),
-      )
-    ]);
+            SafeArea(child: Toolbar(title: "ประวัติทั่วไป",)),
+          ],
+        ),
+        Expanded(
+            child: SingleChildScrollView(
+          child: Container(
+            width: 80 * MediaQuery.of(context).size.width / 100,
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: BlossomText(
+                    "ลักษณะผิว",
+                    size: 16,
+                    color: BlossomTheme.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SkinLookRadioGroup((skin) {
+                  skinType = skin;
+                }),
+                SizedBox(
+                  height: 12,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: BlossomText(
+                    "ลักษณะสิว",
+                    size: 16,
+                    color: BlossomTheme.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                AcneLookRadioGroup((acne) {
+                  acneTypes = acne;
+                }),
+                SizedBox(
+                  height: 12,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: BlossomText(
+                    "เคยรักษาสิว?",
+                    size: 16,
+                    color: BlossomTheme.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                AcneTreat(_acneTreatTextController),
+                SizedBox(
+                  height: 12,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: BlossomText(
+                    "ประวัติแพ้ยา",
+                    size: 16,
+                    color: BlossomTheme.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: BlossomText(
+                    "ยาที่แพ้",
+                    size: 16,
+                    color: BlossomTheme.black,
+                  ),
+                ),
+                DrugAllergy(_drugAllergyTextController),
+                SizedBox(
+                  height: 40,
+                ),
+                ButtonPinkGradient(
+                  "ยืนยัน",
+                  true,
+                  () {
+                    _provider.callServiceRegister(context, widget._profileData, skinType, acneTypes,
+                        _acneTreatTextController.text, _drugAllergyTextController.text);
+                  },
+                  width: 30 * MediaQuery.of(context).size.width / 100,
+                  height: 40,
+                  radius: 6,
+                )
+              ],
+            ),
+          ),
+        )),
+      ]),
+    );
   }
 }
