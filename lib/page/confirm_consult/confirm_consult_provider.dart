e6765ce68fb9043ca84cdda92041565f38cbff8b
SongkhomSitthirat <prew.sitthirat@gmail.com>
2021-04-23 23:22:50 +0700

    Success register, and place order.

--- lib/page/confirm_consult/confirm_consult_provider.dart
+++ lib/page/confirm_consult/confirm_consult_provider.dart
@@ -1,13 +1,21 @@
 import 'package:blossom_clinic/base/base_provider.dart';
+import 'package:blossom_clinic/doctor/doctor_provider.dart';
 import 'package:blossom_clinic/model/available_slot_model.dart';
-import 'package:blossom_clinic/model/base/base_request_model.dart';
 import 'package:blossom_clinic/model/doctor_info_model.dart';
 import 'package:blossom_clinic/model/request/create_appointment_order_model.dart';
 import 'package:blossom_clinic/model/slot_model.dart';
 import 'package:blossom_clinic/model/time_slot_model.dart';
+import 'package:blossom_clinic/page/history/history_provider.dart';
+import 'package:blossom_clinic/page/login/login_provider.dart';
+import 'package:blossom_clinic/page/main/main_page.dart';
+import 'package:blossom_clinic/page/main/main_provider.dart';
+import 'package:blossom_clinic/page/profile/profile_provider.dart';
+import 'package:blossom_clinic/page/service/service_provider.dart';
 import 'package:blossom_clinic/usecase/create_appointment_order_use_case.dart';
 import 'package:blossom_clinic/utils/user_data.dart';
 import 'package:flutter/material.dart';
+import 'package:injector/injector.dart';
+import 'package:provider/provider.dart';
 
 class ConfirmConsultProvider extends BaseProvider with ChangeNotifier {
 
@@ -30,12 +38,42 @@ class ConfirmConsultProvider extends BaseProvider with ChangeNotifier {
         availableSlotModel.id,
         timeSlotModel.id,
         slotModel.id);
-    final result = await _createAppointmentOrderUseCase.execute(BaseRequestModel<CreateAppointmentOrderModel>(requestModel));
+    final result = await _createAppointmentOrderUseCase.execute(requestModel);
     result.whenWithResult((data) {
       Navigator.pop(context);
+      showToast("การจองคิวสำเร็จ");
+      goToMainPage(context);
     }, (map) {
       Navigator.pop(context);
       errorHandle.proceed(context, map);
     });
+  }
+
+  void goToMainPage(BuildContext context) {
+    Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (BuildContext context) {
+      return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (BuildContext context) => MainProvider(),
+          ),
+          ChangeNotifierProvider(
+            create: (BuildContext context) => DoctorProvider(Injector.appInstance.get()),
+          ),
+          ChangeNotifierProvider(
+            create: (BuildContext context) => HistoryProvider(),
+          ),
+          ChangeNotifierProvider(
+            create: (BuildContext context) => ServiceProvider(),
+          ),
+          ChangeNotifierProvider(
+            create: (BuildContext context) => LoginProvider(Injector.appInstance.get(), Injector.appInstance.get()),
+          ),
+          ChangeNotifierProvider(
+            create: (BuildContext context) => ProfileProvider(),
+          ),
+        ],
+        child: MainPage(),
+      );
+    }), (route) => false);
   }
 }
--- lib/usecase/register_use_case.dart
+++ lib/usecase/register_use_case.dart
@@ -4,19 +4,18 @@ import 'package:blossom_clinic/model/request/create_new_application_user_request
 import 'package:blossom_clinic/utils/error_utils.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 
-class RegisterUseCase extends BaseAsyncUseCase<CreateNewApplicationUserRequestModel, Map<String, dynamic>> {
+class RegisterUseCase extends BaseAsyncUseCase<CreateNewApplicationUserRequestModel, dynamic> {
 
   FirebaseFunctions _firebaseFunctions;
 
   RegisterUseCase(this._firebaseFunctions);
 
   @override
-  Future<Result<Map<String, dynamic>>> execute(CreateNewApplicationUserRequestModel parameter) async {
+  Future<Result<dynamic>> execute(CreateNewApplicationUserRequestModel parameter) async {
     try {
       HttpsCallable callable = _firebaseFunctions.httpsCallable("users-createNewApplicationUser");
-      final result = await callable.call(parameter.toJson());
-      print(result.data);
-      return Success(Map());
+      final result = await callable.call(parameter.data.toJson());
+      return Success(result.data);
     } catch (e) {
       print(e);
       return Error(ErrorUtils.getErrorMessage(e));
