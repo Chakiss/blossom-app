335cfab5d3395c6561cdaa1ab473b76a5f6fb595
SongkhomSitthirat <prew.sitthirat@gmail.com>
2021-05-03 03:11:59 +0700

    Handle signout connecty cube when doctor sign out.

--- lib/page/doctor_profile/doctor_profile_provider.dart
+++ lib/page/doctor_profile/doctor_profile_provider.dart
@@ -1,5 +1,6 @@
 import 'package:blossom_clinic/base/base_provider.dart';
 import 'package:blossom_clinic/utils/route_manager.dart';
+import 'package:connectycube_sdk/connectycube_sdk.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -8,6 +9,7 @@ class DoctorProfileProvider extends BaseProvider with ChangeNotifier {
 
   Future<void> logout(BuildContext context) async {
     await FirebaseAuth.instance.signOut();
+    await signOut();
     goToSplashScreen(context);
   }
 
