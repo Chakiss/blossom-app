04b4393595ff84d964015c547bfb86236cfdf02d
SongkhomSitthirat <prew.sitthirat@gmail.com>
2021-05-19 10:29:12 +0700

    Remove debug on top right banner.

--- lib/blossom_clinic_application.dart
+++ lib/blossom_clinic_application.dart
@@ -12,6 +12,7 @@ class BlossomClinicApplication extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         accentColor: BlossomTheme.white,
       ),
